632d43eaca4e32f0565c38a286fc89f8dc41c967
la0rg <lanbinaleksey@gmail.com>
2015-12-05 00:09:42 +0600

    Day4 refactoring

--- Day4/Day4.go
+++ Day4/Day4.go
@@ -7,25 +7,19 @@ import (
 	"io"
 	"strconv"
 	"strings"
-	"time"
 )
 
 func main() {
-
 	m := md5.New()
 	const hash = "yzbqklnj"
-	const MaxInt = int(^uint(0) >> 1)
-	var res int
-
-	start := time.Now()
-	for i := 1; i < MaxInt; i++ {
+	var res int = 1
+	for ; ; res++ {
 		m.Reset()
-		io.WriteString(m, hash+strconv.Itoa(i))
+		io.WriteString(m, hash+strconv.Itoa(res))
 		hx := hex.EncodeToString(m.Sum(nil))
 		if strings.HasPrefix(hx, "000000") {
-			res = i
 			break
 		}
 	}
-	fmt.Printf("%v - %v", res, time.Since(start))
+	fmt.Printf("%v", res)
 }
